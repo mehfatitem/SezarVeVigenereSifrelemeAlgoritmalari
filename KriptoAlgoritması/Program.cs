589d6f022d9be0a5e572102a1f458da62eafe5c8
Mehmed Fatih Temiz <temizfatih54@gmail.com>
2022-06-18 23:54:47 +0300

    Sezar Vigenere Şifrelemesi

---
+++ KriptoAlgoritması/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace KriptoAlgoritmasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kripto sk = new SezarKripto();
+            Kripto vk = new VigenereKripto();
+
+            Console.WriteLine(sk.Sifrele(".MeHmEd,FaTiH,tEmİz.", "11"));
+            Console.WriteLine(sk.Desifrele(".VnRvNm,OiEşR,eNvŞı.", "11")); 
+
+            Console.WriteLine(new SezarKripto().SifrelenmisMetninAnahtariniBul(".MeHmEd,FaTiH,tEmİz.", ".VnRvNm,OiEşR,eNvŞı."));
+
+            Console.WriteLine(vk.Sifrele(".MeHmEd,FaTiH,tEmİz.", "key"));
+            Console.WriteLine(vk.Desifrele(".ZıGzIc,PeSuL,sÖrHj.", "key"));
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ KriptoAlgoritması/SezarKripto.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KriptoAlgoritmasi
+{
+    public class SezarKripto : Kripto
+    {
+        private int indeks;
+        private int yeniIndeks;
+        private int yeniAnahtar;
+        private string sonuc;
+        private List<string> alfabe;
+
+        public override string Sifrele(string sifrelenecekMetin, string anahtar)
+        {
+            return this.Sifrele(sifrelenecekMetin, anahtar, "sifre");
+        }
+        public override string Desifrele(string desifrelenecekMetin, string anahtar)
+        {
+            return this.Sifrele(desifrelenecekMetin, anahtar, "desifre");
+        }
+        private string Sifrele(string metin, string anahtar, string sifrelemeTuru)
+        {
+            sonuc = String.Empty;
+            yeniAnahtar = Convert.ToInt32(anahtar) % alfabeUzunlugu;
+            for (int i = 0; i < metin.Length; i++)
+            {
+                if (char.IsLetter(metin[i]))
+                {
+                    if (char.IsLower(metin[i]))
+                    {
+                        alfabe = KucukAlfabe;
+                    }
+                    else if (char.IsUpper(metin[i]))
+                    {
+                        alfabe = BuyukAlfabe;
+                    }
+
+                    indeks = alfabe.IndexOf(metin[i].ToString());
+
+                    switch (sifrelemeTuru)
+                    {
+                        case "desifre":
+                            yeniIndeks = ((indeks - yeniAnahtar + alfabeUzunlugu) % alfabeUzunlugu);
+                            break;
+                        case "sifre":
+                            yeniIndeks = ((indeks + yeniAnahtar + alfabeUzunlugu) % alfabeUzunlugu);
+                            break;
+                        default:
+                            Console.WriteLine("Hatali şifreleme turu!");
+                            break;
+                    }
+
+                    sonuc += alfabe[yeniIndeks];
+
+                }
+                else
+                {
+                    sonuc += metin[i];
+                }
+            }
+            return sonuc;
+        }
+        public int SifrelenmisMetninAnahtariniBul(string metin, string sifrelenmisMetin)
+        {
+            int anahtar = 0;
+            for (int i = 0; i < metin.Length; i++)
+            {
+                if (char.IsLetter(metin[i]))
+                {
+                    if (char.IsLower(metin[i]))
+                    {
+                        alfabe = KucukAlfabe;
+
+                    }
+                    else if (char.IsUpper(metin[i]))
+                    {
+                        alfabe = BuyukAlfabe;
+                    }
+                    int metinIndeks = alfabe.IndexOf(metin[i].ToString());
+                    int sifrelenmisMetinIndeks = alfabe.IndexOf(sifrelenmisMetin[i].ToString());
+                    anahtar = sifrelenmisMetinIndeks - metinIndeks % alfabeUzunlugu;
+                    break;
+                }
+            }
+            return anahtar;
+        }
+    }
+}
