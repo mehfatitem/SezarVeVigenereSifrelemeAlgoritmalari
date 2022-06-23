47361923ae00717707b0a03304e194fb8bced9b8
Mehmed Fatih Temiz <temizfatih54@gmail.com>
2022-06-24 02:11:20 +0300

    ...

--- KriptoAlgoritması/Helpers/ProgramHelper.cs
+++ KriptoAlgoritması/Helpers/ProgramHelper.cs
@@ -15,6 +15,9 @@ namespace KriptoAlgoritmasi.Helpers
 
         private void Init()
         {
+            var baslangicZamani = DateTime.Now;
+            Yazdir("Sistem başlangıç zamanı " + string.Format("{0:dd/mm/yyyy HH:mm:ss fff}", baslangicZamani));
+            Yazdir(Environment.NewLine);
             base.InitKriptoNesne();
             Yazdir(sk.Sifrele(".MeHmEd,FaTiH,tEmİz.", "11"));
             Yazdir(sk.Desifrele(".VnRvNm,OiEşR,eNvŞı.", "11"));
@@ -23,7 +26,11 @@ namespace KriptoAlgoritmasi.Helpers
 
             Yazdir(vk.Sifrele(".MeHmEd,FaTiH,tEmİz.", "key"));
             Yazdir(vk.Desifrele(".ZıGzIc,PeSuL,sÖrHj.", "key"));
-
+            var bitisZamani = DateTime.Now;
+            Yazdir(Environment.NewLine);
+            Yazdir("Sistem bitiş zamanı " + string.Format("{0:dd/mm/yyyy HH:mm:ss fff}", bitisZamani));
+            TimeSpan diff = bitisZamani - baslangicZamani;
+            Yazdir("Calisma Suresi : " + diff.TotalMilliseconds + " msn");
             Durdur();
         }
     }
